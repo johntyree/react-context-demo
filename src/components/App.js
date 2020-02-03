dacf182774eb007b048874685cfbd1e1e50a60b0
John Tyree <johntyree@gmail.com>
2020-02-03 00:54:59 +0000

    Skeleton setup

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+
+class App extends Component {
+
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us"
+                       onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag nl"
+                       onClick={() => this.onLanguageChange('dutch')}/>
+                </div>
+                <UserCreate />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render() {
+        return (
+            <button className="ui button primary">
+                Submit
+            </button>
+        );
+    }
+}
+
+export default Button;
